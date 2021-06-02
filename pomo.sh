1d0f78535de21cb18ba608d0db330e772d19340a
Arijit Basu <sayanarijit@users.noreply.github.com>
2021-06-02 13:42:47 +0530

    Update pomo.sh

--- pomo.sh
+++ pomo.sh
@@ -8,5 +8,5 @@ min=${arg1:?Example: pomo 15 Take a break}
 sec=$((min * 60))
 msg="${args:?Example: pomo 15 Take a break}"
 while true; do
-    sleep "${sec:?}" && notify-send "${msg:?}"
+    sleep "${sec:?}" && echo "${msg:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
 done
