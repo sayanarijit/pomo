b9f195eed2dadbe92aec06ed5d4acb4374e060ae
Arijit Basu <sayanarijit@gmail.com>
2021-06-08 22:20:39 +0530

    Print date instead of message
    Closes: https://github.com/sayanarijit/pomo/issues/1

--- pomo.sh
+++ pomo.sh
@@ -7,6 +7,7 @@ args="$*"
 min=${arg1:?Example: pomo 15 Take a break}
 sec=$((min * 60))
 msg="${args:?Example: pomo 15 Take a break}"
+
 while true; do
-    sleep "${sec:?}" && echo "${msg:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
+    echo $(date '+%H:%M') && sleep "${sec:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
 done
